36d8b945ace6ebe1c4775fca478f2e0b47d14757
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-06-23 13:15:35 -0500

    7

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:4200', 'https://game-frontend-navy.vercel.app'],
+      origin: ['https://game-frontend-navy.vercel.app'],
       credentials: true,
     },
   });
