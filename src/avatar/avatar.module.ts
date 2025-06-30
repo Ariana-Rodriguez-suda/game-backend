07015aaa4aa5234a6805136231e79257d8d71c7c
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-06-30 07:35:51 -0500

    10

--- src/avatar/avatar.module.ts
+++ src/avatar/avatar.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Avatar } from './avatar.entity';
 import { AvatarService } from './avatar.service';
 import { AvatarController } from './avatar.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Avatar])],
   providers: [AvatarService],
   controllers: [AvatarController],
+  exports: [TypeOrmModule], 
 })
 export class AvatarModule {}
