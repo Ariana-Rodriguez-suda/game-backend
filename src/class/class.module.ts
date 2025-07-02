0f13174901c0836566167d0061c94081ce190247
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-07-02 06:27:47 -0500

    16

--- src/class/class.module.ts
+++ src/class/class.module.ts
@@ -4,9 +4,10 @@ import { Class } from './class.entity';
 import { ClassesService } from './class.service';
 import { ClassesController } from './class.controller';
 import { Teacher } from 'src/users/teacher/teacher.entity';
+import { Player } from 'src/users/player/player.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Class, Teacher])],
+  imports: [TypeOrmModule.forFeature([Class, Teacher, Player])],
   providers: [ClassesService],
   controllers: [ClassesController],
   exports: [ClassesService, TypeOrmModule],
