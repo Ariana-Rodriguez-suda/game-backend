27de9c07409d7f9c3fa67b0321cf8092f62942b2
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-07-04 19:59:37 -0500

    21

--- src/users/player/player.module.ts
+++ src/users/player/player.module.ts
@@ -5,9 +5,10 @@ import { ShopItem } from 'src/shop/shop.entity';
 import { InventoryItem } from 'src/inventory/inventory.entity';
 import { PlayerController } from './player.controller';
 import { PlayerService } from './player.service';
+import { Class } from 'src/class/class.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Player, ShopItem, InventoryItem])],
+  imports: [TypeOrmModule.forFeature([Player, ShopItem, InventoryItem, Class])],
   controllers: [PlayerController],
   providers: [PlayerService],
   exports: [PlayerService, TypeOrmModule],
