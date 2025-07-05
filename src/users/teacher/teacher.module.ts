da3a90302f4ee05c8e793a66238de9c289875e67
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-07-04 20:17:20 -0500

    23

--- src/users/teacher/teacher.module.ts
+++ src/users/teacher/teacher.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Teacher } from './teacher.entity';
 import { TeacherService } from './teacher.service';
 import { TeacherController } from './teacher.controller';
+import { ClassesModule } from 'src/class/class.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Teacher])],
+  imports: [TypeOrmModule.forFeature([Teacher]), 
+            ClassesModule
+],
   providers: [TeacherService],
   controllers: [TeacherController],
   exports: [TeacherService],
